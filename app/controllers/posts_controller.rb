3dc86e81eda3c488b905e3bb41cde7b630ff3484
sleitzel <sleitzel06@gmail.com>
2016-03-07 16:37:11 -0600

    Created Posts controller and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,31 @@
 class PostsController < ApplicationController
 	def index
+		@content_first = "This is some sample text for my App";
+		@content_second = "This is some sample text for my App";
+	end
+
+	def new
 
 	end
+
+	def create
+
+	end
+
+	def edit
+
+	end
+
+	def update
+
+	end
+
+	def show
+
+	end
+
+	def destroy
+
+	end
+	
 end
